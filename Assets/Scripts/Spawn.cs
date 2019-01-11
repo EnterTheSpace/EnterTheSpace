ad27239ec695c78b831751fa8c4c55190fb49bd6
WARMACHINE\Itsuki ichiyuu <itsukiichiyuu@gmail.com>
2019-01-11 14:19:31 +0100

    ajout de scripts mineurs
    ajout de deux scripts:
    -spawn (pour faire spawn un objet à un endroit donné)
    -NextLvl (pour lancer le niveau suivant quand le joueur entre dans une certaine zone)

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+public GameObject SpawningCharacter;
+
+
+	// Use this for initialization
+	void Start () {
+		SpawningCharacter.transform.position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/nextLvl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLvl : MonoBehaviour {
+
+public int next;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("level_"+next);
+        }
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+} 
